51a47922a30322cceec9178d01dc8ed5fa4b49fb
luis acerv <l.arr.cerv@gmail.com>
2018-07-24 12:45:39 -0500

    revert scripts

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const app = express();
-const csp = require('helmet-csp');
-// Since the root/src dir contains our index.html
-app.use(
-	csp({
-		directives: {
-			defaultSrc: ['\'self\'']
-		}
-	})
-);
+
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
 // Falback port will be 8080; basically for pre-production test in localhost
