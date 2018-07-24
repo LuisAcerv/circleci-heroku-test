182bd017751a329511f77f16ee0dbc730c911849
luis acerv <l.arr.cerv@gmail.com>
2018-07-24 12:52:27 -0500

    Update Server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
 
-// Heroku bydefault set an ENV variable called PORT=443
-//  so that you can access your site with https default port.
-// Falback port will be 8080; basically for pre-production test in localhost
-// You will use $ npm run prod for this
-app.listen(process.env.PORT || 8080);
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/dist'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
+
+app.listen(port);
